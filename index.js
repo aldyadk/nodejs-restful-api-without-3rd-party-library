af2ac0e297cc1f312ce54d46f5387a5e0d7fd20a
aldyadk <aldy.andika@gmail.com>
2019-01-18 17:38:22 +0700

    done building a RESTful API

---
+++ index.js
@@ -0,0 +1,13 @@
+const server = require("./lib/server");
+const workers = require("./lib/workers");
+
+const app = {};
+
+app.init = function() {
+  server.init();
+  workers.init();
+};
+
+app.init();
+
+module.exports = app;
